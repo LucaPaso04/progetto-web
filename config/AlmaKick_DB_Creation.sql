3497e3a3572e3cc59b0291cfd4bce3fff81bac61
leo016 <leonardoper04@gmail.com>
2026-08-18 23:32:18 +0200

    Complete DB Creation File
    Completed DB creation file with all the other tables, and switched order of the DROP TABLE for better delete and update of foreign key references

--- config/AlmaKick_DB_Creation.sql
+++ config/AlmaKick_DB_Creation.sql
@@ -62,11 +62,12 @@ lo schema seguito è il seguente:
 CREATE DATABASE IF NOT EXISTS `almakick` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `almakick`;
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `rating`;
+DROP TABLE IF EXISTS `partecipation`;
 DROP TABLE IF EXISTS `friendship`;
 DROP TABLE IF EXISTS `match`;
-DROP TABLE IF EXISTS `partecipation`;
-DROP TABLE IF EXISTS `rating`;
+DROP TABLE IF EXISTS `user`;
+
 
 CREATE TABLE `user` (
     `id_user` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -79,10 +80,120 @@ CREATE TABLE `user` (
     `password` varchar(255) NOT NULL,
     `role` enum('user', 'admin') NOT NULL DEFAULT 'user',
     `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    
+
     UNIQUE (friend_code),
     UNIQUE (username),
     UNIQUE (mail)
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `friendship` (
+    `id_friendship` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id_requester` int NOT NULL,
+    `id_receiver` int NOT NULL,
+    `friendship_status` enum('pending', 'accepted', 'rejected', 'blocked') NOT NULL DEFAULT 'pending',
+    `request_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE (`id_requester`, `id_receiver`),
+    CHECK (`id_requester` <> `id_receiver`),
+
+    /*Per cancellare, e aggiornare i riferimenti dello user 
+    all'id del requester */
+    CONSTRAINT `fk_friendship_requester`
+    FOREIGN KEY (`id_requester`)
+    REFERENCES `user` (`id_user`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+    CONSTRAINT `fk_friendship_receiver`
+    FOREIGN KEY (`id_receiver`)
+    REFERENCES `user` (`id_user`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `match` (
+    `id_match` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id_host` int NOT NULL,
+    `status` enum('programmed', 'playing', 'ended', 'canceled') NOT NULL DEFAULT 'programmed',
+    `date` date NOT NULL,
+    `hour` time NOT NULL,
+    `visibility` enum('public', 'private') NOT NULL DEFAULT 'private',
+    `format` enum('5v5', '7v7', '11v11') NOT NULL DEFAULT '5v5',
+    `place` varchar(100) NOT NULL,
+    `max_players` int NOT NULL,
+    `total_cost` int NOT NULL,
+    `result_team_a` int DEFAULT NULL,
+    `result_team_b` int DEFAULT NULL,
+
+    CONSTRAINT `fk_match_host`
+    FOREIGN KEY (`id_host`)
+    REFERENCES `user` (`id_user`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `partecipation` (
+    `id_partecipation` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id_match` int NOT NULL,
+    `id_user` int NOT NULL,
+    `team` enum('A', 'B') NOT NULL,
+    `status` enum('invited', 'played', 'refused', 'waiting') NOT NULL,
+    `goals` int DEFAULT 0,
+
+    UNIQUE(`id_match`, `id_user`),
+    CHECK (`goals` >= 0),
+
+    CONSTRAINT `fk_match_partecipation`
+    FOREIGN KEY (`id_match`)
+    REFERENCES `match` (`id_match`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+    CONSTRAINT `fk_user_partecipation`
+    FOREIGN KEY (`id_user`)
+    REFERENCES `user` (`id_user`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `rating` (
+    `id_rating` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id_match` int NOT NULL,
+    `id_user_rated` int NOT NULL,
+    `id_user_rating` int NOT NULL,
+    `vote` int NOT NULL,
+    `comment` varchar(200) DEFAULT NULL,
+    `rating_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE(`id_match`, `id_user_rated`, `id_user_rating`),
+    CHECK (`id_user_rated` <> `id_user_rating`),
+    CHECK (`vote` BETWEEN 1 AND 10),
+
+    CONSTRAINT `fk_match_rating`
+    FOREIGN KEY (`id_match`)
+    REFERENCES `match` (`id_match`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+    CONSTRAINT `fk_user_rated`
+    FOREIGN KEY (`id_user_rated`)
+    REFERENCES `user` (`id_user`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+    CONSTRAINT `fk_user_rating`
+    FOREIGN KEY (`id_user_rating`)
+    REFERENCES `user` (`id_user`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 ) ENGINE=InnoDB 
 DEFAULT CHARSET=utf8mb4 
 COLLATE=utf8mb4_unicode_ci;
